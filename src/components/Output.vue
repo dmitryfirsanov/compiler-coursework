b48b674612706a6cf7709edace9751a553324561
Dmitry Firsanov <dmitryfirsanov03@gmail.com>
2023-10-22 22:58:35 +0300

    components: компонент в который выводится результат лексического анализа; позже в него будет выводится другая информация

---
+++ src/components/Output.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <textarea
+      name="output"
+      cols="65"
+      rows="25"
+      class="resize-none border rounded border-black p-3 bg-gray-100 outline-none cursor-default"
+      readonly
+    >{{ displayTokens }}</textarea>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    outputCode: Object,
+  },
+  computed: {
+    displayTokens() {
+      return Object.entries(this.outputCode).map(([key, value]) => {
+        return `${key} : [${value.join(", ")}]`
+      });
+    },
+  },
+}
+</script>
