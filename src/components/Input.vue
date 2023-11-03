83657ea9c0639a87143c47777ba622da226f5448
Dmitry Firsanov <dmitryfirsanov03@gmail.com>
2023-11-03 10:57:14 +0300

    добавил метод парсера

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,7 +2,7 @@
   <div>
     <textarea
       v-model="inputCode"
-      @input="analyzeCode"
+      @input="parser"
       cols="65"
       rows="25"
       class="resize-none border rounded border-black p-3 bg-gray-100  outline-none"
@@ -12,6 +12,8 @@
 </template>
 
 <script>
+import peg from 'pegjs';
+
 export default {
   data() {
     return {
@@ -30,7 +32,7 @@ export default {
       };
 
       const code = this.inputCode;
-      const globalRegex = /program|var|begin|end|integer|real|boolean|read|output|do\swhile|true|false|switch|case|for|let|[a-z]+\d*[a-z]*|\d+|[+\-*/=<>]|\(|\)|,|;|:|:=|\.{1,2}|\{|\}/g;
+      const globalRegex = /program|var|begin|end|integer|real|boolean|read|output|do\swhile|true|false|switch|case|for|let|[a-z]+\d*[a-z]*|\d+|[+\-*/=<>]|\(|\)|,|;|:|:=|!|&&|\|\||\.{1,2}|\{|\}/g;
 
       const matches = code.match(globalRegex);
 
@@ -47,7 +49,7 @@ export default {
         keywords: /^program|var|begin|end|integer|real|boolean|read|output|do\swhile|true|false|switch|case|for|let$/,
         identifier: /^[a-z]\w*$/,
         number: /^\d+$/,
-        separator: /^[{}()=<>;:.,+\-*/]$/,
+        separator: /^[{}()=<>;:.,+\-*/!]$/,
       };
 
       if (regex.keywords.test(token)) {
@@ -56,12 +58,72 @@ export default {
         return 'identifiers';
       } else if (regex.number.test(token)) {
         return 'numbers';
-      } else if (regex.separator.test(token)) {
+      } else if (regex.separator.test(token) || token === "||" || token === "&&") {
         return 'separators';
       } else {
         return 'unknown';
       }
     },
+    parser() {
+      const code = this.outputCode;
+
+      const grammar = `start = программа
+
+буква = [a-z]
+цифра = [0-9]
+пробел = ' '
+
+идентификатор = буква цифра* последовательность_букв*
+последовательность_букв = буква цифра* последовательность_букв?
+число = цифра+
+
+ключевое_слово = "program" / "var" / "begin" / "end" / "integer" / "real" / "boolean" / "read" / "output" / "do while" / "true" / "false" / "switch" / "case" / "for" / "let"
+
+разделитель = '(' / ')' / ',' / ';' / ':' / ':=' / '.' / '{' / '}' / '+' / '-' / '*' / '/' / '||' / '&&' / '!' / '=' / '<' / '>'
+
+тип = "integer" / "real" / "boolean"
+
+программа = "program" "var" описание ":begin" оператор+ "end."
+
+описание = тип идентификатор (',' идентификатор)*
+
+тело = "begin" (оператор ';' )* "end"
+
+оператор = присваивание / условный / фиксированный_цикл / условный_цикл / составной / ввод / вывод
+
+присваивание = ("let")? идентификатор ":=" выражение ";"
+
+условный = "switch" выражение "{" "case" идентификатор ":" оператор "}" 
+
+фиксированный_цикл = "for" "[" выражение ";" выражение ";" выражение "]" оператор
+
+условный_цикл = "do while" выражение оператор "loop"
+
+составной = "begin" оператор+ "end"
+
+ввод = "read" "(" идентификатор (',' идентификатор)* ")"
+
+вывод = "output" "(" выражение (пробел выражение)* ")"
+
+выражение = сумма / произведение / логическая_константа ";"
+
+сумма = произведение ("+" / "-" / "&&") произведение*
+
+произведение = множитель / ("*" / "/" / "||") множитель*
+
+множитель = идентификатор / число / логическая_константа / "!" множитель / "(" выражение ")"
+
+логическая_константа = "true" / "false"
+`;
+
+      const parser = peg.generate(grammar);
+      try {
+        const result = parser.parse(this.inputCode.replace(/\s+/g, ''));
+        console.log(JSON.stringify(result, null, 2));
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
 };
 </script>
